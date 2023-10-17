cc03d8e96e4c9212facda82cba0b7204f00164fc
AlbertProfe <albertprofe@gmail.com>
2023-10-17 10:55:28 +0200

    #feat create some books and add to array

--- src/main/java/com/example/demo/BookService.java
+++ src/main/java/com/example/demo/BookService.java
@@ -13,18 +13,27 @@ public class BookService {
 
         Faker faker = new Faker();
         Book book1 = new Book();
+        Book book2 = new Book();
 
         String author1 = faker.book().author();
         String title1 = faker.book().title();
 
+        String author2 = faker.book().author();
+        String title2 = faker.book().title();
+
         book1.setAuthor(author1);
         book1.setTitle(title1);
 
+        book2.setTitle(title2);
+        book2.setAuthor(author2);
+
         books.add(book1);
+        books.add(book2);
 
     }
 
-
+    // return books to controller
+    // get books form list static from class and return as signature
     public ArrayList<Book> getAllBooks (){
         return books;
     }
